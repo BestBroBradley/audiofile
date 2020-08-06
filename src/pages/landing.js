33bbf4910aff76e76331ee30935864599c9a4f0b
Allison Bradley <allison.bradley@outlook.com>
2020-08-06 11:44:40 -0600

    Wired up buttons on landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Landing() {
     return (
         <>
         <h1>Audiofile</h1>
-        <button><h2>Sign In</h2></button>
-        <button><h2>Sign Up</h2></button>
+        <button><Link to="/signin"><h2>Sign In</h2></Link></button>
+        <button><Link to="/signup"><h2>Sign Up</h2></Link></button>
         </>
     )
 }
